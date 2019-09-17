be7ea702e03243cbb50bf2a07fb91e1409215f90
recognitionone <recognitionone@gmail.com>
2019-09-17 12:10:16 +0200

    WIP adding router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
 import TodoApp from "./TodoApp";
+import { Router, Route } from 'react-router';
+
+
+
+
+const Root = ({ store }) => (
+		<Provider store={store}>
+	    <TodoApp />
+	  </Provider>
+	)
 
 const rootElement = document.getElementById("root");
+
+
 ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
+  <Root store={ store }/>,
   rootElement
 );
